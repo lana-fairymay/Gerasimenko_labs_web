566d6f1da0e0e4ddb7f853d00a7e229df074edcc
Svetlana Gerasimenko <fairyland1405@gmail.com>
2020-10-18 20:56:33 +0300

    Изменен сервер

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (request, result) => {
   result.sendFile(path.join(__dirname+'/index.html'));
@@ -11,4 +11,6 @@ app.get('/', (request, result) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  console.log('Welcome!!!!!');
 })
+
