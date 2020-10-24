9e07a8f6a2475f7d02fc22f2161ff08df05f96e1
lana-fairymay <fairyland1405@gmail.com>
2020-10-24 22:59:07 +0300

    Пофиксила

--- public/js/main.js
+++ public/js/main.js
@@ -17,28 +17,24 @@ function resultTask2(){
 	secondCounterTask2 = parseInt(document.getElementById("secondCounterTask2").value)
 	console.warn(`Ты передал: ${firstCounterTask2}, ${secondCounterTask2}`)
 	var div = document.getElementById('task2')
-	while(div.firstChild) div.removeChild(div.firstChild);
+	deleteText(div)
 	var h4 = document.createElement('h4');
 	h4.innerText = `Результат: ${firstCounterTask2 + secondCounterTask2}`
 	div.append(h4);
 }
 
 function resultTask3(){
-	var arr = new Array(5);
-	arr[0] = parseInt(document.getElementById("First").value)
-	
-	arr[1] = parseInt(document.getElementById("Second").value)
-	arr[2] = parseInt(document.getElementById("Third").value)
-	arr[3] = parseInt(document.getElementById("Forth").value)
-	arr[4] = parseInt(document.getElementById("Fifth").value)
-	for( var  i = 0; i < 5; i++){
-		if (isNaN(arr[i])) arr[i] = 0
-	}
-	var div = document.getElementById('task3')
-	while(div.firstChild) div.removeChild(div.firstChild);
+	var arr = [];
+	var div = document.getElementById("values")
+	console.warn(div.getElementsByTagName("input"))
+	for (i = 0; i < 5; i++)
+	if (isNaN(div.getElementsByTagName("input")[i].value)) arr.push(0)
+	else arr.push(div.getElementsByTagName("input")[i].value)
+	deleteText(document.getElementById("task3"))
 	var h4 = document.createElement('h4');
 	h4.innerText = `Результат: ${arr.map((digit) => digit*digit)}`
-	div.append(h4);
+	document.getElementById("task3").append(h4);
+	console.log(arr.map((digit) => digit*digit))
 }
 
 function InformationTask4() {
@@ -46,8 +42,8 @@ function InformationTask4() {
 	var div2 = document.getElementById('task41')
 	var p1 = document.createElement('p');
 	var p2 = document.createElement('p');
-	while(div1.firstChild) div1.removeChild(div1.firstChild);
-	while(div2.firstChild) div2.removeChild(div2.firstChild);
+	deleteText(div1)
+	deleteText(div2)
 	p2.innerText = `МГТУ СТАНКИН \n 09.03.01 \n Программное обеспечение средств вычислительной техники и автоматизированных систем`
 	p1.innerText = `University \n Spetialty \n Direction `
 	div1.append(p1);
@@ -55,7 +51,11 @@ function InformationTask4() {
 }
 function InformationTask41() {
 	var div = document.getElementById('task4')
-	while(div.firstChild) div.removeChild(div.firstChild);
+	deleteText(div)
 	var div = document.getElementById('task41')
+	deleteText(div)
+}
+
+function deleteText(div) {
 	while(div.firstChild) div.removeChild(div.firstChild);
 }
